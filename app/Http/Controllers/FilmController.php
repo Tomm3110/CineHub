2aab7079c5c4fc685793e4ae70bc2d519bdbd977
Tom <tom_firmin@ens.unvi-artois.fr>
2025-11-13 10:16:14 +0100

    methode index for filtre and view

--- app/Http/Controllers/FilmController.php
+++ app/Http/Controllers/FilmController.php
@@ -3,11 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use app\Models\Film;
 
 class FilmController extends Controller
 {
     public function index() {
-        // $films = null;
-        return '<h1>FilmController index</h1>';
+        $cat = request()->input('cat', "All");
+        $query = Film::query();
+        if ($cat != 'All') {
+            $query->where('categorie', $cat);
+        }
+        $films = $query->get();
+        $query = Film::query();
+        $cats = $query->distinct()->pluck('categorie');
+        return view('films.index', ['titre'=>"Liste des films",'cat'=>$cat,'categories' => $cats ,'films' => $films]);
     }
 }
