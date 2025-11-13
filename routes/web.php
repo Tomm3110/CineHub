91f915a214b0c4c8b105afd60732ea6ea6623242
wissem.nacer <wissem.nacer@info0130.ipa.iutlens.local>
2025-11-13 09:38:32 +0100

    Ajout regles de routage question 2

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('pages.accueil');
+})->name('accueil');
+
+Route::get('/films', function () {
+    return view('pages.films');
+})->name('films');
+
+Route::get('/series', function () {
+    return view('pages.series');
+})->name('series');
+
+Route::get('/contact', function () {
+    return view('pages.contact');
+})->name('contact');
