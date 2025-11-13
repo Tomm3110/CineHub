762347a60a91e6efca45237b5ce80ec854a2aa98
Tom BILLET <tom_billet@ens.univ-artois.fr>
2025-11-13 09:46:17 +0100

    adding file FilmController

---
+++ app/Http/Controllers/FilmController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FilmController extends Controller
+{
+    public function index() {
+        // $films = null;
+        return '<h1>FilmController index</h1>';
+    }
+}
