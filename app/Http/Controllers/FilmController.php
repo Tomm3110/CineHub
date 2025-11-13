2c3e9524205c4380a000951a1057a54d04be96f9
Tom <tom_firmin@ens.unvi-artois.fr>
2025-11-13 11:23:26 +0100

    add cookie for research

--- app/Http/Controllers/FilmController.php
+++ app/Http/Controllers/FilmController.php
@@ -4,18 +4,31 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Film;
+use Illuminate\Support\Facades\Cookie;
 
 class FilmController extends Controller
 {
-    public function index() {
-        $cat = request()->input('cat', "");
+    public function index(Request $request) {
+        $cat = $request->input('cat');
+        if ($cat === null) {
+            $cat = $request->cookie('cat');
+        }
+        if (!$cat) {
+            $cat = 'All';
+        }
+        Cookie::queue('cat', $cat, 10);
         $query = Film::query();
-        if ($cat != '') {
+        if ($cat != 'All' && $cat != '') {
             $query->where('titre', $cat);
         }
         $films = $query->get();
-        $query = Film::query();
-        $cats = $query->distinct()->pluck('titre');
-        return view('films.index', ['titre'=>"Liste des films",'cat'=>$cat,'titres' => $cats ,'films' => $films]);
+        $titres = Film::distinct()->pluck('titre');
+
+        return view('films.index', [
+            'titre' => "Liste des films",
+            'cat' => $cat,
+            'titres' => $titres,
+            'films' => $films
+        ]);
     }
 }
