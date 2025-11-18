4cc8dc4e10894ed0584fec6aedb3e9a9fb1b464c
Tom <tom_firmin@ens.unvi-artois.fr>
2025-11-18 16:35:21 +0100

    adding CRUD

--- app/Http/Controllers/FilmController.php
+++ app/Http/Controllers/FilmController.php
@@ -31,4 +31,51 @@ class FilmController extends Controller
             'films' => $films
         ]);
     }
+
+    public function show($id) {
+        $film = Film::findOrFail($id);
+        return view('films.show', compact('film'));
+    }
+
+    public function create() {
+        return view('films.create'); // Affiche le formulaire [cite: 80]
+    }
+
+    public function store(Request $request) {
+        // 1. Validation
+        $request->validate([
+            'title' => 'required|string|max:255',
+            // Ajoutez ici les autres règles
+        ]);
+
+        // 2. Création
+        $film = new Film();
+        $film->title = $request->input('title');
+        $film->media = $request->input('media'); // Le nouveau champ
+        // ... autres champs
+        $film->save();
+
+        // 3. Redirection avec message flash
+        return redirect()->route('films.index')
+            ->with('success', 'Film créé avec succès');
+    }
+
+    public function edit($id) {
+        $film = Film::findOrFail($id);
+        return view('films.edit', compact('film'));
+}
+
+    public function update(Request $request, $id) {
+        // Validation (similaire au store)
+        $request->validate([
+            'title' => 'required|string|max:255',
+        ]);
+
+        $film = Film::findOrFail($id);
+        $film->title = $request->input('title');
+        // ... mise à jour des autres champs
+        $film->save();
+
+        return redirect()->route('films.index')->with('success', 'Film mis à jour');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ Route::get('/home', function () {
     return view('accueil');
 })->name('accueil');
 
-Route::get('/films', [FilmController::class, 'index'])->name('films');
+Route::resource('films', FilmController::class);
 
 Route::get('/series', function () {
     return view('series');
