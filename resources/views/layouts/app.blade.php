5f42a941acf2c1034944b605f9792e388bb831aa
enzo.fantasia <enzo.fantasia@info0727.ipa.iutlens.local>
2025-11-27 09:35:40 +0100

    modification du layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,13 +4,13 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>@yield('title', 'Mon Application CinéHub')</title>
-        <script src="https://cdn.tailwindcss.com"></script>
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="bg-gray-100 text-gray-900 font-sans antialiased">
-
+        <x-header></x-header>
         <div class="container mx-auto p-6">
             @yield('content')
         </div>
-
+        <x-footer></x-footer>
     </body>
 </html>
