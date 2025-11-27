9be16b6ca3a7bfcceede732d3af96c26e0874706
enzo.fantasia <enzo.fantasia@info0622.ipa.iutlens.local>
2025-11-27 12:37:38 +0100

    correction bug

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 import './bootstrap';
+
+// Import Alpine.js
+import Alpine from 'alpinejs';
+window.Alpine = Alpine;
+Alpine.start();
+
+// Import Font Awesome
+import '@fortawesome/fontawesome-free/js/all.js';
