68c572b1a998e01e7f32ea0511e3a9ac5ec01151
enzo.fantasia <enzo.fantasia@info0622.ipa.iutlens.local>
2025-11-27 10:33:31 +0100

    add success & errors

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,20 @@
     </head>
     <body class="bg-gradient-to-b from-red-950 via-red-900 to-black text-white min-h-screen flex flex-col p-6">
         <x-header></x-header>
-        <div class="container mx-auto p-6">
+        <div class="container mx-auto p-6 mt-10">
+            @if(session('success'))
+                <div class="mb-4 p-3 bg-green-600/50 text-green-200 rounded">{{ session('success') }}</div>
+            @endif
+
+            @if($errors->any())
+                <div class="mb-4 p-3 bg-red-800/40 text-red-200 rounded">
+                    <ul class="list-disc pl-5">
+                        @foreach($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
             @yield('content')
         </div>
         <x-footer></x-footer>
