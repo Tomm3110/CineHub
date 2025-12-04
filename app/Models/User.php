512270a810cf3a3716eb0e891d6ff3adfc12c746
Tom <tom_firmin@ens.unvi-artois.fr>
2025-12-04 09:38:19 +0100

    generate password notification in User

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,7 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use App\Notifications\CustomResetPasswordNotification
+use App\Notifications\CustomResetPasswordNotification;
 
 class User extends Authenticatable
 {
