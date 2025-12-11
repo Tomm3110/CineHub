64d5c355a3291b5927a3478f83a4d258c96bf1d1
Tom <tom_firmin@ens.unvi-artois.fr>
2025-12-11 12:56:46 +0100

    adding migrations, seeders, factory, models for acteurs, genre, participe

---
+++ database/migrations/2025_12_11_124416_create_participe_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\Acteur;
+use App\Models\Film;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('participe', function (Blueprint $table) {
+            $table->foreignIdFor(Film::class)->constrained()->onDelete('cascade');
+            $table->foreignIdFor(Acteur::class)->constrained()->onDelete('cascade');
+            $table->primary(['film_id', 'acteur_id']);
+            $table->float('note');
+            $table->String('role');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('participe');
+    }
+};
---
+++ database/seeders/ActeurSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ActeurSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
